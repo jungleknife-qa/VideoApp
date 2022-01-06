3c1cf48b387c9f89a25f7c2656b38f611f5d8f0d
Tony Marchetto <antony.marchetto@digital.hmrc.gov.uk>
2022-01-06 16:37:30 +0000

    CRUD services created for Movies

---
+++ VideoApp/Services/IMovieService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoApp.Models;
+
+namespace VideoApp.Services
+{
+    public interface IMovieService
+    {
+        Movie AddMovie(Movie movie);
+
+        List<Movie> GetMovies();
+
+        void UpdateMovie(Movie movie);
+
+        void DeleteMovie(int Id);
+
+        Movie GetMovie(int Id);
+    }
+}
