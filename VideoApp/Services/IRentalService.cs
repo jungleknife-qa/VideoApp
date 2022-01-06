49bba92d9814744c62f904c7f804852f1c4b7725
Tony Marchetto <antony.marchetto@digital.hmrc.gov.uk>
2022-01-06 17:13:24 +0000

    CRUD services created for Rental table

---
+++ VideoApp/Services/IRentalService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoApp.Models;
+
+namespace VideoApp.Services
+{
+    public interface IRentalService
+    {
+        Rental AddRental(Rental rental);
+
+        List<Rental> GetRental();
+
+        void UpdateRental(Rental rental);
+
+        void DeleteRental(int Id);
+
+        Rental GetRental(int Id);
+    }
+}
---
+++ VideoApp/Services/RentalService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoApp.Models;
+using VideoApp.DBContexts;
+
+namespace VideoApp.Services
+{
+    public class RentalService : IRentalService
+    {
+        public MyDBContext _myDbContext;
+        public RentalService(MyDBContext myDbContext)
+        {
+            _myDbContext = myDbContext;
+        }
+        public Rental AddRental(Rental rental)
+        {
+            _myDbContext.Rentals.Add(rental);
+            _myDbContext.SaveChanges();
+            return rental;
+        }
+        public List<Rental> GetRental()
+        {
+            return _myDbContext.Rentals.ToList();
+        }
+        public void UpdateRental(Rental rental)
+        {
+            _myDbContext.Rentals.Update(rental);
+            _myDbContext.SaveChanges();
+        }
+        public void DeleteRental(int Id)
+        {
+            var rental = _myDbContext.Rentals.FirstOrDefault(x => x.Id == Id);
+            if (rental != null)
+            {
+                _myDbContext.Remove(rental);
+                _myDbContext.SaveChanges();
+            }
+        }
+        public Rental GetRental(int Id)
+        {
+            return _myDbContext.Rentals.FirstOrDefault(x => x.Id == Id);
+        }
+    }
+}
