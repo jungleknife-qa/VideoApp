672da378969458a76082d80d516deddac8dc301f
Tony Marchetto <antony.marchetto@digital.hmrc.gov.uk>
2022-01-07 13:33:17 +0000

    Customer controller updated for CRUD operations

--- VideoApp/Controllers/CustomerController.cs
+++ VideoApp/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using VideoApp.DBContexts;
 using VideoApp.Models;
+using VideoApp.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,17 +14,56 @@ namespace VideoApp.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        private MyDBContext myDbContext;
-
-        public CustomerController(MyDBContext context)
+        private readonly ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
         {
-            myDbContext = context;
+            _customerService = customerService;
         }
 
         [HttpGet]
-        public IList<Customer> Get()
+        [Route("[action]")]
+        [Route("api/Customer/GetCustomers")]
+        public IEnumerable<Customer> GetCustomers()
         {
-            return (this.myDbContext.Customers.ToList());
+            return _customerService.GetCustomers();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        [Route("api/Customer/AddCustomer")]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            _customerService.AddCustomer(customer);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        [Route("api/Customer/UpdateCustomer")]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            _customerService.UpdateCustomer(customer);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("[action]")]
+        [Route("api/Customer/DeleteCustomer")]
+        public IActionResult DeleteCustomer(int id)
+        {
+            var existingCustomer = _customerService.GetCustomer(id);
+            if (existingCustomer != null)
+            {
+                _customerService.DeleteCustomer(existingCustomer.Id);
+                return Ok();
+            }
+            return NotFound($"Customer not found with ID : {existingCustomer.Id}");
+        }
+        [HttpGet]
+        [Route("GetCustomer")]
+        public Customer GetCustomer(int id)
+        {
+            return _customerService.GetCustomer(id);
         }
     }
 }
